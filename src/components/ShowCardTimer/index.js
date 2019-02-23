95dc5315f785ac220ac9fc57c2111733bb58c004
Gabe R <rodriguezgab1601@gmail.com>
2019-02-23 19:10:25 +0800

    Finalize the timer logic

--- src/components/ShowCardTimer/index.js
+++ src/components/ShowCardTimer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { addDays, distanceInWordsToNow } from "date-fns";
+import { addDays, differenceInSeconds } from "date-fns";
 
 /**
  * Tries to find what date it will be for a particular day of the week.
@@ -26,26 +26,98 @@ function getDateWithDayOfTheWeek(day, hour, minute) {
 
   return result;
 }
+function convertSecondsToTimer(seconds) {
+  let temp, hours, minutes;
+  let output = "";
+
+  temp = convertSecondsToHours(seconds);
+  seconds = temp.seconds;
+  hours = temp.result;
+
+  temp = convertSecondsToMinutes(seconds);
+  seconds = temp.seconds;
+  minutes = temp.result;
+
+  if (hours) {
+    output = `${output} ${paddedTime(hours)}:`;
+  }
+  output = `${output}${paddedTime(minutes)}:${paddedTime(seconds)}`;
+
+  return output;
+}
+
+function paddedTime(t) {
+  if (t < 10) {
+    return `0${t}`;
+  } else {
+    return `${t}`;
+  }
+}
+
+function convertSecondsToDays(seconds) {
+  return convertSecondsToInterval(seconds, 60 * 60 * 24);
+}
+
+function convertSecondsToHours(seconds) {
+  return convertSecondsToInterval(seconds, 60 * 60);
+}
+
+function convertSecondsToMinutes(seconds) {
+  return convertSecondsToInterval(seconds, 60);
+}
+
+function convertSecondsToInterval(seconds, interval) {
+  const result = Math.floor(seconds / interval);
+
+  if (result) {
+    seconds = seconds - result * interval;
+  }
+
+  return { seconds: seconds, result: result };
+}
+function calculateDistance(date) {
+  const now = new Date();
+
+  let result = differenceInSeconds(date, now);
+
+  return convertSecondsToTimer(result);
+}
 
 class ShowCardTimer extends Component {
   constructor(props) {
     super(props);
 
+    const airDate = getDateWithDayOfTheWeek(
+      props.air_day,
+      props.air_hour,
+      props.air_minute
+    );
+
     this.state = {
       flicker: false,
-      distance: distanceInWordsToNow(
-        getDateWithDayOfTheWeek(
-          props.air_day,
-          props.air_hour,
-          props.air_minute
-        ),
-        { includeSeconds: true, addSuffix: true }
-      )
+      airDate: airDate,
+      currentDistance: calculateDistance(airDate),
+      currentInterval: null
     };
+
+    this.countdownTimer = this.countdownTimer.bind(this);
+  }
+  componentDidMount() {
+    const interval = setInterval(() => {
+      this.countdownTimer(this.state.airDate);
+    }, 1000);
+    this.setState({
+      currentInterval: interval
+    });
+  }
+  countdownTimer(date) {
+    this.setState({
+      currentDistance: calculateDistance(date)
+    });
   }
 
   render() {
-    return <span>{this.state.distance}</span>;
+    return <span>{this.state.currentDistance}</span>;
   }
 }
 
