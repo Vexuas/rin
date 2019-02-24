e346d40d64234ea5f2cc5f0534e23aae23bef094
Gabe R <rodriguezgab1601@gmail.com>
2019-02-24 15:47:35 +0800

    Updates

---
+++ precache-manifest.77abbcd6e60d6d1b2d652642397877d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94140d482a4899ce2ffd",
+    "url": "/rin/static/js/runtime~main.94140d48.js"
+  },
+  {
+    "revision": "beb86e5c2a44862d9df4",
+    "url": "/rin/static/js/main.beb86e5c.chunk.js"
+  },
+  {
+    "revision": "1bcbaab0950f39847d36",
+    "url": "/rin/static/js/2.1bcbaab0.chunk.js"
+  },
+  {
+    "revision": "beb86e5c2a44862d9df4",
+    "url": "/rin/static/css/main.0b7516d3.chunk.css"
+  },
+  {
+    "revision": "8dea74dc8ebc55290b92618e7ed10fef",
+    "url": "/rin/index.html"
+  }
+];
